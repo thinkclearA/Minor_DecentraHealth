38881c95c9f3a7925e36ec2d100c3f0c56df1b47
Aaditya Mishra <codewithaadi31@gmail.com>
2023-10-15 18:44:17 +0530

    Removed Some Functionality

--- frontend/src/components/Creators/Creators.jsx
+++ frontend/src/components/Creators/Creators.jsx
@@ -6,11 +6,8 @@ export default function Creators() {
     <>
 		<Box style={{margin: '20% auto 0 auto', width: '50%',textAlign:'center'}}>
 			<Typography variant='h5' fontWeight={'600'}>Aaditya Mishra</Typography>
-			<Typography variant='h5' fontWeight={'600'}>Aditya Jha</Typography>
-			<Typography variant='h5' fontWeight={'600'}>Pranshu chandra</Typography>
-			<Typography variant='h5' fontWeight={'600'}>Prince Kumar</Typography>
-			<Typography variant='h5' fontWeight={'600'}>Shaurya Singh</Typography>
-			<Typography variant='h5' fontWeight={'600'}>Yash Shrivastav</Typography>
+			<Typography variant='h5' fontWeight={'600'}>Amith Harish</Typography>
+			<Typography variant='h5' fontWeight={'600'}>Shubhanshu Kakkar</Typography>
 		</Box>
     </>
   )
